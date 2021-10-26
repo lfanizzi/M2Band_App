4554fefc2a815b297525702761680fe6c04a02fe
Team Greenies Senior Design <lfanizzi@udel.edu>
2021-10-26 15:37:17 -0400

    Initial Commit

---
+++ M2Band/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M2Band
+//
+//  Created by Luke Fanizzi on 10/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M2Band/M2BandApp.swift
@@ -0,0 +1,17 @@
+//
+//  M2BandApp.swift
+//  M2Band
+//
+//  Created by Luke Fanizzi on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct M2BandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
